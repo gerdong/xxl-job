b9d58e6319ca353dc359a47590f570576fe6f96d
gerdong <tuoshu@sina.com>
2022-10-27 11:56:27 +0800

    python3支持

--- xxl-job-core/src/main/java/com/xxl/job/core/glue/GlueTypeEnum.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/glue/GlueTypeEnum.java
@@ -8,7 +8,8 @@ public enum GlueTypeEnum {
     BEAN("BEAN", false, null, null),
     GLUE_GROOVY("GLUE(Java)", false, null, null),
     GLUE_SHELL("GLUE(Shell)", true, "bash", ".sh"),
-    GLUE_PYTHON("GLUE(Python)", true, "python", ".py"),
+    GLUE_PYTHON("GLUE(Python2)", true, "python2", ".py"),
+    GLUE_PYTHON3("GLUE(Python3)", true, "python3", ".py"),
     GLUE_PHP("GLUE(PHP)", true, "php", ".php"),
     GLUE_NODEJS("GLUE(Nodejs)", true, "node", ".js"),
     GLUE_POWERSHELL("GLUE(PowerShell)", true, "powershell", ".ps1");
